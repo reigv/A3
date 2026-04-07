358302ffe5f6220accdb3d5016b4d638323f932e
rhht <reig.vaeo.soiv099@gmail.com>
2026-04-07 11:01:50 -0400

    1.2

---
+++ server1.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { parse } = require('node:path');
+const app = express();
+const port = 3300;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false })); // get info from forms
+
+let TASKS = [];
+
+app.get('/', (req, res) => {
+    res.render('home.ejs', { tasks: TASKS });
+});
+
+app.post('/add-task', (req, res) => {
+    const task_from_form = req.body.home_add_task;
+
+    // add info to the task
+    const new_task = {
+        id: TASKS.length, // keep stack on top of each other
+        name: task_from_form,
+        isCompleted: false
+    }
+
+    TASKS.push(new_task);
+    res.redirect('/');
+});
+
+app.post('/update-task/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    // get check box value from form
+    const is_checked_box = req.body.home_check_box  === 'on'; // check box value is "on" when checked, undefined when unchecked
+
+    for (let i = 0; i < TASKS.length; i++) {
+        if (TASKS[i].id === id) {
+            TASKS[i].isCompleted = is_checked_box;
+            break; 
+        }
+    }
+    res.redirect('/');
+});
+
+
+app.post('/completed-tasks', (req, res) => {
+    let completed_tasks = [];
+    for (let i = 0; i < TASKS.length; i++) {
+        if (TASKS[i].isCompleted) {
+            completed_tasks.push(TASKS[i]);
+        }
+    }
+    res.render('completed_tasks.ejs', { tasks: completed_tasks });
+});
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
